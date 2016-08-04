b2a61b724aedf8052128ff9c377179be0b3a3e84
shashwatsahai <ssahai97@gmail.com>
2016-08-05 04:06:37 +0530

    Update tictac.cpp

--- tictac.cpp
+++ tictac.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
+#include<windows.h>
 using namespace std;
 int count=0;
 int check(int board[][3],int cur)
-{int i,j,k,rowchk,colchk,diachk;
+{int i,j,k,rowchk=0,colchk=0,diachk=0;
     for(i=0;i<3;i++)
         if((board[i][0]==cur)&&(board[i][1]==cur)&&(board[i][2]==cur))
             rowchk=1;
@@ -20,11 +21,11 @@ int check(int board[][3],int cur)
 char sym(int a)
 {
     if(a==1)
-        return('X');
-    else if(a==2)
         return('O');
-    else if(a==0)
+    else if(a==2)
         return(' ');
+    else if(a==0)
+        return('X');
 }
 void disp(int board[][3])
 {int i,j,k;
@@ -51,30 +52,103 @@ cout<<"\n";
     }
 
 }
+int aii=-1,aij=-1;
+int checkwin(int tboard[3][3],int cur)
+{static int winf=0;
 
+    int i,j;
+    for(i=0;i<3;i++)
+    {   for(j=0;j<3;j++)
+        {   if(tboard[i][j]==2)
+                {tboard[i][j]=cur;
+
+                }
+            else
+                continue;
+            if(check(tboard,cur)==1)
+                {
+                    aii=i+1;
+                    aij=j+1;
+                    return winf;
+                }
+            else
+                {
+                    checkwin(tboard,cur);
+                    winf++;
+                }
+    //            tboard[i][j]=2;
+        }
+    }
+}
+int aili=-2,ailj=-2;
+ int checkloss(int tboard[3][3],int cur)
+{static int los=0;
+static int taili=-2,tailj=-2;
+ int i,j;
+    for(i=0;i<3;i++)
+    {   for(j=0;j<3;j++)
+        {   if(tboard[i][j]==2)
+                {
+                    tboard[i][j]=cur;
+                }
+            else
+                continue;
+            if(check(tboard,cur)==1)
+                {
+                    aili=i+1;
+                    ailj=j+1;
+                    return los;
+                }
+            else
+                {
+                    checkloss(tboard,cur);
+                    los++;
+                }
+                tboard[i][j]=2;
+        }
+    }
+}
+int loslvl,winlvl;
+int checkminimax(int tboard[3][3],int cur)
+{
+    loslvl=checkloss(tboard,!cur);
+    winlvl=checkwin(tboard,cur);
+}
 int main()
-{int board[3][3]={0},p1i,p1j,p2i,p2j,i,j,k,win=0;
+{int board[3][3]={2},tb[3][3],p1i,p1j,p2i,p2j,i,j,k,win=0;
+ for(i=0;i<3;i++)
+    for(j=0;j<3;j++)
+    board[i][j]=2;
     while(win!=1&&count<10)
     {
+        system("cls");
         disp(board);
         y:cout<<"\nEnter player 1";
         cin>>p1i>>p1j;
         count++;
         p1i--;
         p1j--;
-         if(board[p1i][p1j]==0)
-            board[p1i][p1j]=1;
+         if(board[p1i][p1j]==2)
+            board[p1i][p1j]=0;
          else{
             cout<<"\nAlready filled try again";
+            count--;
             goto y;
              }
-        board[p1i][p1j]=1;
-        win=check(board,1);
+        //board[p1i][p1j]=1;
+        win=check(board,0);
         if(win)
             {cout<<"\nPLAYER 1 WON";
             disp(board);
             return 1;
             }
+        else if(count==9)
+        {
+            disp(board);
+            cout<<"\nDraw";
+            return 0;
+        }
+        system("cls");
         disp(board);
         if(count>9)
         {
@@ -82,17 +156,28 @@ int main()
             return 0;
         }
         x:cout<<"\nEnter player 2";
+        for(i=0;i<3;i++)
+            for(j=0;j<3;j++)
+            tb[i][j]=board[i][j];
+        checkminimax(tb,1);
+        cout<<"\nLoss acc to  minimax i and j"<<aili<<ailj<<"\nLoss lvl= "<<loslvl;
+       cout<<"\nWIn Acc to minimax i and j"<<aii<<aij<<"\nWin lvl= "<<winlvl<<"\n";
         cin>>p2i>>p2j;
         count++;
         p2i--;
         p2j--;
-        if(board[p2i][p2j]==0)
-        board[p2i][p2j]=2;
+        if(board[p2i][p2j]==2)
+        board[p2i][p2j]=1;
         else{
         cout<<"\nAlready filled try again";
         goto x;
         }
-        win=check(board,2);
+        cout<<"Actual";
+        for(i=0;i<3;i++)
+            for(j=0;j<3;j++)
+                cout<<board[i][j];
+        win=check(board,1);
+        cout<<"Win"<<win;
         if(win)
            { cout<<"\nPLAYER 2 WON";
              disp(board);
@@ -101,4 +186,3 @@ int main()
 
     }
 }
-
